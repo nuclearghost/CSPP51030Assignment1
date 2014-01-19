454420b7985b1c9fe4e14afde7b3487bbdc1122a
Mark Meyer <mark.meyer37@gmail.com>
2014-01-18 18:17:33 -0600

    Initial commit for assignment 1

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -14,6 +14,8 @@
 
 @implementation HelloWorldViewController
 
+@synthesize userName = _userName;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,4 +28,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+
+    NSArray *helloArray = @[@"Hello", @"Hallo", @"Hola", @"God dag", @"Ciao",
+                            @"Salve", @"Salut"];
+    uint32_t rand = arc4random_uniform([helloArray count]);
+    NSString *randomHello = [helloArray objectAtIndex:rand];
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"%@ %@!",
+                          randomHello, nameString];
+    
+    self.label.text = greeting;
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)theTextField {
+    if (theTextField == self.textField) {
+        [self changeGreeting:NULL];
+        [theTextField resignFirstResponder];
+    }
+    return YES;
+}
 @end
